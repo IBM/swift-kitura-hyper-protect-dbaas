20b2aa63a54e763a028aa7217e83c0d5594b6f61
IBM Developer Experience <devrtdo@us.ibm.com>
2018-12-20 17:51:09 +0000

    Automated Update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "swiftkiturahyperprotectdbaas",
     dependencies: [
       .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.5.0")),
-      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
+      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
@@ -13,7 +13,9 @@ let package = Package(
     ],
     targets: [
       .target(name: "swiftkiturahyperprotectdbaas", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health","MongoKitten",      ]),
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics", "Health", "MongoKitten", 
+
+      ]),
 
       .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
     ]
